f3e7ee5efc18a1c52d0a45d4b5e2b4ad982c94b7
Ben Cochran <bcochran@gmail.com>
2015-11-08 02:05:05 -0800

    Initial Commit

---
+++ KaleidoscopeLang/KaleidoscopeLang.h
@@ -0,0 +1,19 @@
+//
+//  KaleidoscopeLang.h
+//  KaleidoscopeLang
+//
+//  Created by Ben Cochran on 11/8/15.
+//  Copyright © 2015 Ben Cochran. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for KaleidoscopeLang.
+FOUNDATION_EXPORT double KaleidoscopeLangVersionNumber;
+
+//! Project version string for KaleidoscopeLang.
+FOUNDATION_EXPORT const unsigned char KaleidoscopeLangVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KaleidoscopeLang/PublicHeader.h>
+
+
