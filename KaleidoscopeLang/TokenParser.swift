dfa9c0b8a27bfb1eee2b4cfcd76ed0c6744e57a9
Ben Cochran <bcochran@gmail.com>
2015-11-09 23:36:29 -0800

    Update lexer to use more of Madness’s string combinators

--- KaleidoscopeLang/TokenParser.swift
+++ KaleidoscopeLang/TokenParser.swift
@@ -13,36 +13,47 @@ import Prelude
 private typealias TokenParser = Parser<String.CharacterView, Token>.Function
 internal typealias TokenArrayParser = Parser<String.CharacterView, [Token]>.Function
 
+// Characters
+private let whitespace: CharacterParser = space <|> tab
+private let lower: CharacterParser = oneOf("a"..."z")
+private let upper: CharacterParser = oneOf("A"..."Z")
+private let alpha: CharacterParser = lower <|> upper
+private let op: CharacterParser = char("+") <|> char("-") <|> char("*") <|> char("/")
+private let paren: CharacterParser =  char("(") <|> char(")")
+
+// Identifiers
+private let identifierStart: CharacterParser = alpha <|> char("_")
+private let identifierFinish: CharacterParser = identifierStart <|> digit
+private let identifierCharacters: CharacterArrayParser = prepend <^> identifierStart <*> identifierFinish*
+private let identifierString: StringParser = String.init <^> identifierCharacters
+
+// Tokens
+private let identifier: TokenParser = Token.Identifier <^> identifierString
+private let character: TokenParser = Token.Character <^> (op <|> paren)
+private let def: TokenParser = const(.Def) <^> %"def"
+private let extern: TokenParser = const(.Extern) <^> %"extern"
+private let number: TokenParser = Token.Number <^> Madness.number
+private let token = def <|> extern <|> identifier <|> number <|> character
+
+internal let tokens: TokenArrayParser = (token <* whitespace*)* <* newline|?
+
+
+// MARK: Public
+
+
+public func tokenizeTopLevelExpression(string: String) -> Either<Error, [Token]> {
+    return parse(tokens, input: string).mapLeft(Error.TokenizeError)
+}
+
+
+// MARK: Private helpers
+
+
 // Stolen from Madness internals
 private func prepend<T>(value: T) -> [T] -> [T] {
     return { [value] + $0 }
 }
 
-private let newline = %"\n"
-private let whitespace = %" " <|> %"\t"
-private let lower = %("a"..."z")
-private let upper = %("A"..."Z")
-private let digit = %("0"..."9")
-private let alpha = lower <|> upper
-private let digits = { $0.joinWithSeparator("") } <^> digit+
-private let op = %"+" <|> %"-" <|> %"*" <|> %"/"
-private let paren =  %"(" <|> %")"
-
-private let def: TokenParser = const(.Def) <^> %"def"
-private let extern: TokenParser = const(.Extern) <^> %"extern"
-private let identifierStart = alpha <|> %"_"
-private let identifierFinish = identifierStart <|> digit
-private let identifierCharacters = prepend <^> identifierStart <*> identifierFinish*
-private let identifierString = { $0.joinWithSeparator("") } <^> identifierCharacters
-private let identifier: TokenParser = Token.Identifier <^> identifierString
-private let character: TokenParser = Token.Character <^> (Character.init <^> (op <|> paren))
-
-private let number: TokenParser = Token.Number <^> ({ Double($0)! } <^> digits)
-private let token = def <|> extern <|> identifier <|> number <|> character
-internal let tokens: TokenArrayParser = (token <* whitespace*)* <* newline|?
-
-// MARK: Public
-
-public func tokenizeTopLevelExpression(string: String) -> Either<Error, [Token]> {
-    return parse(tokens, input: string).mapLeft(Error.TokenizeError)
+private func oneOf<I: IntervalType where I.Bound == Character>(interval: I) -> CharacterParser {
+    return satisfy { interval.contains($0) }
 }
